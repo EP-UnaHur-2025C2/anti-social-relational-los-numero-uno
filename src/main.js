daefd637d8ca646f0961c255ad2350cc667f2398
Nahuel Lautaro Jimenez <nahuellautaro.jimenez@estudiantes.unahur.edu.ar>
2025-10-05 16:05:41 -0300

    Agrego estructura del proyecto

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log("UnaHur - Anti-Social net");
+const express = require("express");
+const app = express();
+const db = require("../db/models");
+//const router = require('')
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+//app.use('', router)
+
+app.listen(PORT, async () => {
+  console.log(`El servidor esta corriendo en el puerto ${PORT}`);
+  await db.sequelize.sync();
+});
