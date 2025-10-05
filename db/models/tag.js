884c7955e25c0eaf94a134529877bc74b8531ccd
BraianR2706 <braianezequiel.rios@estudiantes.unahur.edu.ar>
2025-10-05 18:42:26 -0300

    Primer avance de CRUD en tag y postTag

---
+++ db/models/tag.js
@@ -0,0 +1,29 @@
+
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Tag extends Model {
+    static associate(models) {
+      // Un Tag puede estar relacionado con muchos Posts a través de PostTag (Relación Muchos a Muchos)
+      Tag.hasMany(models.PostTag, {
+        foreignKey: 'TagID',
+        as: 'postTags' 
+      });
+    }
+  }
+  Tag.init({
+    
+    Nombre: {
+      type: DataTypes.STRING,
+      allowNull: false, // El nombre no puede ser nulo
+      unique: true      // (UK)
+    }
+  }, {
+    sequelize,
+    modelName: 'Tag',
+
+    timestamps: false, 
+  });
+  return Tag;
+};
